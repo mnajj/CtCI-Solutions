5e0e41adc057fd0faa56ddee2323e8ddfe2a3a28
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-06-05 21:55:04 +0200

    Create 1.1_IsUnique.cs

---
+++ 1- Arrays and Strings/1.1_IsUnique.cs
@@ -0,0 +1,38 @@
+/// <summary>
+/// Implement an algorithm to determine if a string has all unique characters.
+/// </summary>
+
+string str = "#117";
+Console.WriteLine(IsUnique(str));
+
+bool IsUnique(string str)
+{
+	Dictionary<char, bool> dict = new();
+	foreach (char ch in str)
+	{
+		if (dict.ContainsKey(ch))
+		{
+			return false;
+		}
+		dict[ch] = true;
+	}
+	return true;
+}
+
+/// <summary>
+///  What if you cannot use additional data structures?
+/// </summary>
+bool IsUnique2(string str)
+{
+	var flag = 0;
+	foreach (var ch in str)
+	{
+		var val = ch - 'a';
+		if ((flag & (1 << val)) > 0)
+		{
+			return false;
+		}
+		flag |= (1 << val);
+	}
+	return true;
+}
