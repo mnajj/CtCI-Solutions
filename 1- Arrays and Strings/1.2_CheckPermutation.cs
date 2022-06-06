e3cb5c35541f4c82a722fd9a93d36ed529d5f2c7
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-06-06 04:31:45 +0200

    1.2

---
+++ 1- Arrays and Strings/1.2_CheckPermutation.cs
@@ -0,0 +1,61 @@
+﻿/// <summary>
+/// Given two strings, write a method to decide if one is a permutation of the other.
+/// </summary>
+
+// Solution 1 [Sorting]
+string sort(string str)
+{
+	char[] chars = str.ToCharArray();
+	Array.Sort(chars);
+	return new string(chars);
+}
+
+bool IsPermutation(string str1, string str2)
+{
+	if (str1.Length != str2.Length)
+	{
+		return false;
+	}
+
+	return sort(str1).Equals(sort(str2));
+}
+
+// Solution 2 [Count Lookup]
+bool IsPermutation2(string str1, string str2)
+{
+	if (str1.Length != str2.Length)
+	{
+		return false;
+	}
+
+	var letterCount = new Dictionary<char, int>();
+	foreach (char ch in str1)
+	{
+		if (letterCount.ContainsKey(ch))
+		{
+			letterCount[ch]++;
+		}
+		else
+		{
+			letterCount[ch] = 1;
+		}
+	}
+
+	foreach (char ch in str2)
+	{
+		if (letterCount.ContainsKey(ch))
+		{
+			letterCount[ch]--;
+			if (letterCount[ch] < 0)
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CtCI-Solutions
+![img](https://www.crackingthecodinginterview.com/uploads/6/5/2/8/6528028/header_images/1435811319.jpg)
